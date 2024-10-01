a6b43a1f576e6d578c1398951d8ef29c269e675d
NoNormalCreeper <2560359315@qq.com>
2024-10-01 21:17:05 +0800

    初步实现 console 的基本功能

--- src/Serial/boost_serial_console.cpp
+++ src/Serial/boost_serial_console.cpp
@@ -1,11 +1,49 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/serial_port.hpp>
 #include <string>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     std::string port_name = "/dev/ttyUSB1";
 
     boost::asio::io_context io_context;
-    boost::asio::serial_port serial_port(io_context, port_name);
+    boost::asio::serial_port serial(io_context, port_name);
+
+    serial.set_option(boost::asio::serial_port::baud_rate(115200));
+    serial.set_option(boost::asio::serial_port::character_size(8));
+    serial.set_option(boost::asio::serial_port::stop_bits(
+        boost::asio::serial_port::stop_bits::one));
+    serial.set_option(boost::asio::serial_port::parity(
+        boost::asio::serial_port::parity::none));
+    serial.set_option(boost::asio::serial_port::flow_control(
+        boost::asio::serial_port::flow_control::none));
+
+    if (!serial.is_open()) return 1;
+
+    while (1) {
+        std::string message = "";
+        std::cout << "Send > ";
+        std::cin >> message;
+        // std::cout << std::endl;
+
+        boost::system::error_code ec;
+
+        serial.write_some(boost::asio::buffer(message), ec);
+        if (ec) {
+            std::cerr << "Error when writing to " << port_name << std::endl;
+        }
+        else {
+            std::cout << "Writing '" << message << "' to " << port_name
+                      << std::endl;
+        }
+
+        std::array<char, 1024> responseBuffer;
+        while (1) {
+            auto size = serial.read_some(boost::asio::buffer(responseBuffer));
+            std::string response(responseBuffer.data(), size);
+            std::cout << "Response < " << response << std::endl;
+            break;
+        }
+    }
 }
