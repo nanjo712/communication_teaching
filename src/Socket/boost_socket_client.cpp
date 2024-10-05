b992747da47df2a491561772bf0458ddf3c37731
ng <3076877237@qq.com>
2024-10-05 11:39:26 +0800

    add echo

--- src/Socket/boost_socket_client.cpp
+++ src/Socket/boost_socket_client.cpp
@@ -2,6 +2,7 @@
 #include <boost/asio/read.hpp>
 #include <iostream>
 #include<stdio.h>
+#include<cstdlib>
 
 int main()
 {
@@ -13,18 +14,21 @@ int main()
     std::string message = "time";
     socket.write_some(boost::asio::buffer(message));
     std::string response;
-    response.resize(1024);
+   response.resize(1024);
     socket.read_some(boost::asio::buffer(response));
     std::cout << "Response: " << response << std::endl;
-//我开始写
+//从这里开始
+//发送功能
 char yonghushuru[128];
-printf("enter:");
+ printf("enter:");
 std::cin.getline(yonghushuru,128);
 socket.write_some(boost::asio::buffer(yonghushuru));
 response.resize(1024);
 socket.read_some(boost::asio::buffer(response));
     std::cout << "Response: " << response << std::endl;
-
+ //退出功能    
+if(response=="EXIT")
+exit(0);
 printf("send:%s",yonghushuru);
 
 
